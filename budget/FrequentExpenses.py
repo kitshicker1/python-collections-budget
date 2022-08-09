94bf28d35f51689d2937b5fb54b5c29241048fb0
kitshicker1 <102931778+kitshicker1@users.noreply.github.com>
2022-08-08 21:11:23 -0500

    FrequentExpenses 8/8/22

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,8 @@
+from . import Expense
+
+
+expenses = Expense.Expense()
+expenses.read_expenses(data/spending_data.csv)
+spending_catagories[]:
+    for expense in expenses.list:
+        append(expense.category = spending_categories)
